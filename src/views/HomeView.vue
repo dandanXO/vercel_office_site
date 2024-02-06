9b7a6e5e4962f02c66deb519c595116e319c9104
Frank <frank@ncfc.cc>
2024-02-07 02:24:43 +0800

    refactor: 拆分

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import { Vue3Marquee } from 'vue3-marquee'
-import { Vue3Lottie } from 'vue3-lottie'
-
 import { imgUrl } from '@/assets'
-// import { DotLottieVue as DotLottie } from '@lottiefiles/dotlottie-vue'
-
 import IconEarth from '@icons/nd/earth.svg'
-import IconCursor from '@icons/nd/cursor.svg'
-import IconArrowRight from '@icons/nd/arrow-right.svg'
-import lottie from '@/assets/lottie/home1.json'
+import HomeSection1 from './HomeView/components/HomeSection1.vue'
 
 const logoWhite = imgUrl('logo-white.svg')
 const mainNav = [
@@ -16,14 +9,6 @@ const mainNav = [
   { name: '產品', to: '/product' },
   { name: '人才招募', to: '/career' },
   { name: '聯絡我們', to: '/contact' },
-]
-const marqueeList = [
-  imgUrl('home/knock.png'),
-  imgUrl('home/zapier.png'),
-  imgUrl('home/dribbble.png'),
-  imgUrl('home/do.png'),
-  imgUrl('home/arc.png'),
-  imgUrl('home/byte-dance.png'),
 ]
 </script>
 
@@ -44,50 +29,7 @@ const marqueeList = [
     </div>
   </header>
   <main>
-    <!-- Section 1 -->
-    <div class="container">
-      <div class="mt-100px flex justify-between">
-        <div class="fade-in-bottom w-3/5">
-          <div class="font-rammetto-one text-56px font-400">New Digit Defines</div>
-          <div class="font-rammetto-one mt-14px text-56px font-400">
-            Your&nbsp;
-            <span class="border-6px border-primary border-rounded-40px border-solid px-24px py-8px">
-              Digits
-            </span>
-            &nbsp;.
-            <IconCursor class="ml-26px"></IconCursor>
-          </div>
-          <div class="mt-40px text-40px font-700">探索新數位，重新定義未來</div>
-        </div>
-        <div class="w-2/5">
-          <!-- 官方的還有bug -->
-          <!-- <dot-lottie :src="lottie" class="h-304px w-359px" autoplay loop></dot-lottie> -->
-          <Vue3Lottie
-            :animation-data="lottie"
-            :height="304"
-            :width="359"
-            auto-play
-            loop
-          ></Vue3Lottie>
-        </div>
-      </div>
-      <div>
-        <div className="mt-10 flex items-center gap-x-6">
-          <a
-            href="#"
-            class="rounded-20px bg-primary px-24px py-16px text-17px text-black font-500 decoration-none shadow-sm hover:bg-primary/70 focus-visible:outline-2 focus-visible:outline-primary/70 focus-visible:outline-offset-2 focus-visible:outline"
-          >
-            瞭解更多 <IconArrowRight class="inline"></IconArrowRight>
-          </a>
-        </div>
-      </div>
-      <div class="mt-100px">
-        <Vue3Marquee>
-          <img v-for="(item, index) in marqueeList" :key="index" :src="item" class="mx-40px" />
-        </Vue3Marquee>
-      </div>
-    </div>
-    <!-- Section 1 End -->
+    <HomeSection1 />
     <!-- Section 2 -->
     <div class="bg-neutral-07 relative mt-87px py-150px">
       <!-- <div
@@ -118,38 +60,4 @@ const marqueeList = [
   <footer></footer>
 </template>
 
-<style lang="scss" scoped>
-.fade-in-bottom {
-  -webkit-animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-  animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-}
-/**
- * ----------------------------------------
- * animation fade-in-bottom
- * ----------------------------------------
- */
-@-webkit-keyframes fade-in-bottom {
-  0% {
-    -webkit-transform: translateY(50px);
-    transform: translateY(50px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    opacity: 1;
-  }
-}
-@keyframes fade-in-bottom {
-  0% {
-    -webkit-transform: translateY(50px);
-    transform: translateY(50px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    opacity: 1;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView/components/HomeSection1.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { Vue3Marquee } from 'vue3-marquee'
+import { Vue3Lottie } from 'vue3-lottie'
+// import { DotLottieVue as DotLottie } from '@lottiefiles/dotlottie-vue'
+
+import { imgUrl } from '@/assets'
+
+import IconCursor from '@icons/nd/cursor.svg'
+import IconArrowRight from '@icons/nd/arrow-right.svg'
+import lottie from '@/assets/lottie/home1.json'
+
+const marqueeList = [
+  imgUrl('home/knock.png'),
+  imgUrl('home/zapier.png'),
+  imgUrl('home/dribbble.png'),
+  imgUrl('home/do.png'),
+  imgUrl('home/arc.png'),
+  imgUrl('home/byte-dance.png'),
+]
+</script>
+
+<template>
+  <div class="container">
+    <div class="mt-100px flex justify-between">
+      <div class="fade-in-bottom w-3/5">
+        <div class="font-rammetto-one text-56px font-400">New Digit Defines</div>
+        <div class="font-rammetto-one mt-14px text-56px font-400">
+          Your&nbsp;
+          <span class="border-6px border-primary border-rounded-40px border-solid px-24px py-8px">
+            Digits
+          </span>
+          &nbsp;.
+          <IconCursor class="ml-26px"></IconCursor>
+        </div>
+        <div class="mt-40px text-40px font-700">探索新數位，重新定義未來</div>
+      </div>
+      <div class="w-2/5">
+        <!-- 官方的還有bug -->
+        <!-- <dot-lottie :src="lottie" class="h-304px w-359px" autoplay loop></dot-lottie> -->
+        <Vue3Lottie :animation-data="lottie" :height="304" :width="359" auto-play loop></Vue3Lottie>
+      </div>
+    </div>
+    <div>
+      <div className="mt-10 flex items-center gap-x-6">
+        <a
+          href="#"
+          class="rounded-20px bg-primary px-24px py-16px text-17px text-black font-500 decoration-none shadow-sm hover:bg-primary/70 focus-visible:outline-2 focus-visible:outline-primary/70 focus-visible:outline-offset-2 focus-visible:outline"
+        >
+          瞭解更多 <IconArrowRight class="inline"></IconArrowRight>
+        </a>
+      </div>
+    </div>
+    <div class="mt-100px">
+      <Vue3Marquee>
+        <img v-for="(item, index) in marqueeList" :key="index" :src="item" class="mx-40px" />
+      </Vue3Marquee>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.fade-in-bottom {
+  -webkit-animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+  animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+}
+</style>
