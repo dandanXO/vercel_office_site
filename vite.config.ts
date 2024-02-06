17dffe3fb10c88c4ff5d0c1d2543a327e210de7e
Frank Sung <ysyavv@gmail.com>
2024-02-06 14:04:01 +0800

    chore: stylelint, commitlint

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
   plugins: [
     vue(),
-    vueJsx()
+    vueJsx(),
     // !process.env.VITEST ? Checker({ vueTsc: true }) : undefined
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
