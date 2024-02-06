4bac318272d61364f5b261e0d04e3384768e4ff4
Frank Sung <ysyavv@gmail.com>
2024-02-06 17:11:03 +0800

    chore: unocss

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // import Checker from 'vite-plugin-checker'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    UnoCSS(),
     // !process.env.VITEST ? Checker({ vueTsc: true }) : undefined
   ],
   resolve: {
