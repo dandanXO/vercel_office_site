27cd881e97268867617818e810fbe209ce980511
Frank <frank@ncfc.cc>
2024-02-07 11:58:32 +0800

    feat: section1 mobile

--- src/views/HomeView/components/HomeSection1.vue
+++ src/views/HomeView/components/HomeSection1.vue
@@ -21,27 +21,33 @@ const marqueeList = [
 
 <template>
   <div class="container">
-    <div class="mt-100px flex justify-between">
-      <div class="fade-in-bottom w-3/5">
-        <div class="text-56px font-400 font-rammetto-one">New Digit Defines</div>
-        <div class="mt-14px text-56px font-400 font-rammetto-one">
+    <div class="mt-60px flex flex-wrap lg:mt-100px lg:justify-between">
+      <div class="fade-in-bottom w-full text-center lg:w-3/5">
+        <div class="text-28px font-400 font-rammetto-one lg:text-56px">New Digit Defines</div>
+        <div class="mt-14px text-28px font-400 font-rammetto-one lg:text-56px">
           Your&nbsp;
-          <span class="border-6px border-primary border-rounded-40px border-solid px-24px py-8px">
+          <span
+            class="border-4px border-primary border-rounded-40px border-solid px-16px py-6px lg:border-6px lg:px-24px lg:py-8px"
+          >
             Digits
           </span>
           &nbsp;.
-          <IconCursor class="ml-26px"></IconCursor>
+          <IconCursor
+            class="ml-12px inline h-36px w-36px lg:ml-26px lg:h-64px lg:w-64px"
+          ></IconCursor>
         </div>
-        <div class="mt-40px text-40px font-700">探索新數位，重新定義未來</div>
+        <div class="mt-24px text-26px font-700 lg:mt-40px lg:text-40px">
+          探索新數位，重新定義未來
+        </div>
       </div>
-      <div class="w-2/5">
+      <div class="mt-40px w-full lg:mt-0 lg:w-2/5">
         <!-- 官方的還有bug -->
         <!-- <dot-lottie :src="lottie" class="h-304px w-359px" autoplay loop></dot-lottie> -->
         <Vue3Lottie :animation-data="lottie" :height="304" :width="359" auto-play loop></Vue3Lottie>
       </div>
     </div>
     <div>
-      <div className="mt-10 flex items-center gap-x-6">
+      <div className="mt-10 flex justify-center lg:justify-start items-center gap-x-6">
         <a
           href="#"
           class="rounded-20px bg-primary px-24px py-16px text-17px text-black font-500 decoration-none shadow-sm hover:bg-primary/70 focus-visible:outline-2 focus-visible:outline-primary/70 focus-visible:outline-offset-2 focus-visible:outline"
@@ -59,6 +65,9 @@ const marqueeList = [
 </template>
 
 <style lang="scss" scoped>
+.title {
+  @apply text-28px font-400 font-rammetto-one lg:text-56px;
+}
 .fade-in-bottom {
   -webkit-animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
   animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
