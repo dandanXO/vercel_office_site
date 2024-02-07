41e125be3dedb6acc864d58acd37440a9d1ec5a2
Frank <frank@ncfc.cc>
2024-02-07 12:17:15 +0800

    feat: change breakpoint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -35,12 +35,12 @@ const mainNav = [
     <div class="relative mt-87px bg-neutral-07 py-150px">
       <img class="absolute bottom-0 left-0" :src="imgUrl('home/2-bg-1.png')" />
       <img class="absolute right-0 top-0" :src="imgUrl('home/2-bg-2.png')" />
-      <div class="relative z-1 hidden text-center text-40px text-white font-700 lg:block">
+      <div class="relative z-1 hidden text-center text-40px text-white font-700 xl:block">
         定義全新的企業數字 DNA
       </div>
-      <div class="relative z-1 flex flex-wrap justify-between gap-60px lg:mt-80px lg:gap-0">
+      <div class="relative z-1 flex flex-wrap justify-between gap-60px xl:mt-80px xl:gap-0">
         <div
-          class="w-full text-center lg:w-1/3"
+          class="w-full text-center xl:w-1/3"
           v-for="(item, index) in [
             { num: '5', sub: '國家' },
             { num: '100+', sub: '客戶' },
@@ -59,7 +59,7 @@ const mainNav = [
 
   <footer class="border-t-2px border-white border-solid">
     <div class="flex flex-wrap justify-between py-128px container">
-      <div class="flex flex-col gap-32px lg:flex-row lg:gap-80px">
+      <div class="flex flex-col gap-32px xl:flex-row xl:gap-80px">
         <img :src="logoWhite" class="h-64px w-64px" />
         <div class="flex flex-col gap-18px text-neutral-07">
           <div class="text-16px font-700">關於我們</div>
@@ -73,11 +73,11 @@ const mainNav = [
           <div class="text-14px font-400">隱私權政策</div>
         </div>
       </div>
-      <div class="mt-64px w-full flex flex-col gap-18px lg:mt-0">
+      <div class="mt-64px w-full flex flex-col gap-18px xl:mt-0">
         <div class="text-16px text-neutral-07 font-700">訂閱電子報</div>
         <div class="text-14px text-neutral-06 font-400">第一時間掌握我們的消息！</div>
         <form
-          class="flex justify-between border-1 border-neutral-04 rounded-20px border-solid bg-neutral-01 px-20px py-8px lg:w-360px"
+          class="flex justify-between border-1 border-neutral-04 rounded-20px border-solid bg-neutral-01 px-20px py-8px xl:w-360px"
         >
           <label for="email-address" class="sr-only">你的信箱</label>
           <input
--- src/views/HomeView/components/HomeSection1.vue
+++ src/views/HomeView/components/HomeSection1.vue
@@ -21,33 +21,33 @@ const marqueeList = [
 
 <template>
   <div class="container">
-    <div class="mt-60px flex flex-wrap lg:mt-100px lg:justify-between">
-      <div class="fade-in-bottom w-full text-center lg:w-3/5">
-        <div class="text-28px font-400 font-rammetto-one lg:text-56px">New Digit Defines</div>
-        <div class="mt-14px text-28px font-400 font-rammetto-one lg:text-56px">
+    <div class="mt-60px flex flex-wrap xl:mt-100px xl:justify-between">
+      <div class="fade-in-bottom w-full text-center xl:w-3/5">
+        <div class="text-28px font-400 font-rammetto-one xl:text-56px">New Digit Defines</div>
+        <div class="mt-14px text-28px font-400 font-rammetto-one xl:text-56px">
           Your&nbsp;
           <span
-            class="border-4px border-primary border-rounded-40px border-solid px-16px py-6px lg:border-6px lg:px-24px lg:py-8px"
+            class="border-4px border-primary border-rounded-40px border-solid px-16px py-6px xl:border-6px xl:px-24px xl:py-8px"
           >
             Digits
           </span>
           &nbsp;.
           <IconCursor
-            class="ml-12px inline h-36px w-36px lg:ml-26px lg:h-64px lg:w-64px"
+            class="ml-12px inline h-36px w-36px xl:ml-26px xl:h-64px xl:w-64px"
           ></IconCursor>
         </div>
-        <div class="mt-24px text-26px font-700 lg:mt-40px lg:text-40px">
+        <div class="mt-24px text-26px font-700 xl:mt-40px xl:text-40px">
           探索新數位，重新定義未來
         </div>
       </div>
-      <div class="mt-40px w-full lg:mt-0 lg:w-2/5">
+      <div class="mt-40px w-full xl:mt-0 xl:w-2/5">
         <!-- 官方的還有bug -->
         <!-- <dot-lottie :src="lottie" class="h-304px w-359px" autoplay loop></dot-lottie> -->
         <Vue3Lottie :animation-data="lottie" :height="304" :width="359" auto-play loop></Vue3Lottie>
       </div>
     </div>
     <div>
-      <div className="mt-10 flex justify-center lg:justify-start items-center gap-x-6">
+      <div className="mt-10 flex justify-center xl:justify-start items-center gap-x-6">
         <a
           href="#"
           class="rounded-20px bg-primary px-24px py-16px text-17px text-black font-500 decoration-none shadow-sm hover:bg-primary/70 focus-visible:outline-2 focus-visible:outline-primary/70 focus-visible:outline-offset-2 focus-visible:outline"
