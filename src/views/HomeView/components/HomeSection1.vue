2fa14166de3101f1bc042f3bcc1b722923f87d90
Frank <frank@ncfc.cc>
2024-02-20 02:01:02 +0800

    feat: rewrite

--- src/views/HomeView/components/HomeSection1.vue
+++ src/views/HomeView/components/HomeSection1.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { Vue3Marquee } from 'vue3-marquee'
-import { Vue3Lottie } from 'vue3-lottie'
 // import { DotLottieVue as DotLottie } from '@lottiefiles/dotlottie-vue'
 
 import { imgUrl } from '@/assets'
@@ -8,6 +7,9 @@ import { imgUrl } from '@/assets'
 import IconChavronUp from '@icons/nd/chavron-up.svg'
 import IconVector1 from '@icons/nd/vector1.svg'
 import IconVector2 from '@icons/nd/vector2.svg'
+import { breakpointsTailwind } from '@vueuse/core'
+
+const { xl } = useBreakpoints(breakpointsTailwind)
 
 const sectionRef = ref(null)
 const { width: eW, height: eH } = useElementSize(sectionRef)
@@ -22,9 +24,31 @@ const marqueeList = [
 </script>
 
 <template>
-  <div class="relative overflow-hidden pb-63px pt-160px" ref="sectionRef">
+  <div class="relative overflow-hidden pb-63px pt-160px xl:pt-125px" ref="sectionRef">
     <div class="mt-60px flex flex-wrap container xl:mt-100px xl:justify-between">
-      <div class="mb-64px w-full text-center">
+      <div v-if="xl" class="mb-72px w-full text-center">
+        <div class="text-80px font-900 leading-94px tracking-[4%] font-raleway">
+          New Digit Defines <IconVector1 class="inline h-48px w-48px"></IconVector1>
+        </div>
+        <div class="mb-16px w-full text-center">
+          <span class="text-80px font-900 leading-94px tracking-[4%] font-raleway"
+            >Your <IconVector2 class="inline h-48px w-43px"></IconVector2
+          ></span>
+
+          <span
+            class="relative text-80px text-primary-hover font-900 leading-94px tracking-[4%] after:absolute after:right-0 after:top-50% after:h-72px after:transform after:border-r-7px after:border-neutral-07 after:rounded-4px after:content-empty after:-translate-y-1/2"
+          >
+            Future
+          </span>
+          <span
+            class="px-10 text-80px text-neutral-07 font-900 leading-94px tracking-[4%] font-raleway"
+          >
+            .
+          </span>
+        </div>
+        <div class="heading-3">探索新數位，重新定義未來</div>
+      </div>
+      <div class="mb-64px w-full text-center" v-else>
         <div class="text-40px font-900 leading-46px tracking-[4%] font-raleway">New Digit</div>
         <div class="my-4 text-40px font-900 leading-46px tracking-[4%] font-raleway">
           Defines <IconVector1 class="inline h-40px w-40px"></IconVector1> Your
@@ -48,48 +72,93 @@ const marqueeList = [
     </div>
 
     <div class="mt-124px">
-      <Vue3Marquee :duration="15" gradient gradientLength="15%">
+      <Vue3Marquee :duration="15" gradient gradientLength="15%" direction="reverse">
         <img v-for="(item, index) in marqueeList" :key="index" :src="item" class="mx-40px" />
       </Vue3Marquee>
     </div>
 
-    <div
-      class="absolute top-0 flex transform items-center justify-center -left-50% -translate-y-1/2"
-    >
-      <div class="placeholder" :style="{ width: eW + 'px', height: eW + 'px' }"></div>
+    <template v-if="xl">
       <div
-        class="dashed-circle-bottom !border-primary"
-        :style="{ width: eW + 'px', height: eW + 'px' }"
-      ></div>
+        class="absolute left-0 top-0 flex transform items-center justify-center -z-1 -translate-x-1/3 -translate-y-1/3"
+      >
+        <div class="placeholder" :style="{ width: 603 + 'px', height: 603 + 'px' }"></div>
+        <div
+          class="dashed-circle-bottom !border-primary"
+          :style="{ width: 603 + 'px', height: 603 + 'px' }"
+        ></div>
+        <div
+          class="dashed-circle-bottom !border-neutral-04"
+          :style="{
+            width: 421 + 'px',
+            height: 421 + 'px',
+          }"
+        ></div>
+      </div>
       <div
-        class="dashed-circle-bottom !border-neutral-04"
-        :style="{
-          width: eW * 0.7 + 'px',
-          height: eW * 0.7 + 'px',
-        }"
-      ></div>
-    </div>
-    <div class="absolute flex transform items-center justify-center -right-50% -translate-y-1/2">
-      <div class="placeholder" :style="{ width: eW + 'px', height: eW + 'px' }"></div>
+        class="absolute right-0 flex translate-x-1/2 transform items-center justify-center -z-1 -translate-y-1/2"
+      >
+        <div class="placeholder" :style="{ width: 768 + 'px', height: 768 + 'px' }"></div>
+        <div
+          class="dashed-circle-bottom !border-neutral-04"
+          :style="{ width: 768 + 'px', height: 768 + 'px' }"
+        ></div>
+        <div
+          class="dashed-circle-bottom !border-primary"
+          :style="{
+            width: 568 + 'px',
+            height: 568 + 'px',
+          }"
+        ></div>
+        <div
+          class="dashed-circle-bottom !border-neutral-04"
+          :style="{
+            width: 368 + 'px',
+            height: 368 + 'px',
+          }"
+        ></div>
+      </div>
+    </template>
+    <template v-else>
       <div
-        class="dashed-circle-bottom !border-neutral-04"
-        :style="{ width: eW + 'px', height: eW + 'px' }"
-      ></div>
+        class="absolute top-0 flex transform items-center justify-center -left-50% -z-1 -translate-y-1/2"
+      >
+        <div class="placeholder" :style="{ width: eW + 'px', height: eW + 'px' }"></div>
+        <div
+          class="dashed-circle-bottom !border-primary"
+          :style="{ width: eW + 'px', height: eW + 'px' }"
+        ></div>
+        <div
+          class="dashed-circle-bottom !border-neutral-04"
+          :style="{
+            width: eW * 0.7 + 'px',
+            height: eW * 0.7 + 'px',
+          }"
+        ></div>
+      </div>
       <div
-        class="dashed-circle-bottom !border-primary"
-        :style="{
-          width: eW * 0.8 + 'px',
-          height: eW * 0.8 + 'px',
-        }"
-      ></div>
-      <div
-        class="dashed-circle-bottom !border-neutral-04"
-        :style="{
-          width: eW * 0.6 + 'px',
-          height: eW * 0.6 + 'px',
-        }"
-      ></div>
-    </div>
+        class="absolute flex transform items-center justify-center -right-50% -z-1 -translate-y-1/2"
+      >
+        <div class="placeholder" :style="{ width: eW + 'px', height: eW + 'px' }"></div>
+        <div
+          class="dashed-circle-bottom !border-neutral-04"
+          :style="{ width: eW + 'px', height: eW + 'px' }"
+        ></div>
+        <div
+          class="dashed-circle-bottom !border-primary"
+          :style="{
+            width: eW * 0.8 + 'px',
+            height: eW * 0.8 + 'px',
+          }"
+        ></div>
+        <div
+          class="dashed-circle-bottom !border-neutral-04"
+          :style="{
+            width: eW * 0.6 + 'px',
+            height: eW * 0.6 + 'px',
+          }"
+        ></div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -104,12 +173,12 @@ const marqueeList = [
 }
 
 .dashed-circle-bottom {
-  @apply w-190px h-190px;
+  @apply w-100px h-100px;
   border: 2px dashed;
   border-radius: 50%; /* 使邊框呈現圓形 */
   position: absolute;
   animation: rotateAnimation 150s linear infinite; /* 應用旋轉動畫，每次動畫持續 50 秒，無限次重復 */
   margin: 20px; /* 為了更好的展示效果，添加一些外邊距 */
-  z-index: -1;
+  z-index: -2;
 }
 </style>
