bd873c1bbf4f114c4c5fe7318899283a80c5c6c2
Frank <frank@ncfc.cc>
2024-02-20 01:19:55 +0800

    feat: mobile

--- src/views/HomeView/components/HomeSection1.vue
+++ src/views/HomeView/components/HomeSection1.vue
@@ -5,10 +5,12 @@ import { Vue3Lottie } from 'vue3-lottie'
 
 import { imgUrl } from '@/assets'
 
-import IconCursor from '@icons/nd/cursor.svg'
-import IconArrowRight from '@icons/nd/arrow-right.svg'
-import lottie from '@/assets/lottie/home1.json'
+import IconChavronUp from '@icons/nd/chavron-up.svg'
+import IconVector1 from '@icons/nd/vector1.svg'
+import IconVector2 from '@icons/nd/vector2.svg'
 
+const sectionRef = ref(null)
+const { width: eW, height: eH } = useElementSize(sectionRef)
 const marqueeList = [
   imgUrl('home/knock.png'),
   imgUrl('home/zapier.png'),
@@ -20,61 +22,94 @@ const marqueeList = [
 </script>
 
 <template>
-  <div class="container">
-    <div class="mt-60px flex flex-wrap xl:mt-100px xl:justify-between">
-      <div class="fade-in-bottom w-full text-center xl:w-3/5 xl:text-start">
-        <div class="font-rammetto-one text-28px font-400 xl:text-56px">New Digit Defines</div>
-        <div class="font-rammetto-one mt-14px text-28px font-400 xl:text-56px">
-          Your&nbsp;
-          <span
-            class="border-4px border-primary border-rounded-40px border-solid px-16px py-6px xl:border-6px xl:px-24px xl:py-8px"
-          >
-            Digits
-          </span>
-          &nbsp;.
-          <IconCursor
-            class="ml-12px inline h-36px w-36px xl:ml-26px xl:h-64px xl:w-64px"
-          ></IconCursor>
+  <div class="relative overflow-hidden pb-63px pt-160px" ref="sectionRef">
+    <div class="mt-60px flex flex-wrap container xl:mt-100px xl:justify-between">
+      <div class="mb-64px w-full text-center">
+        <div class="text-40px font-900 leading-46px tracking-[4%] font-raleway">New Digit</div>
+        <div class="my-4 text-40px font-900 leading-46px tracking-[4%] font-raleway">
+          Defines <IconVector1 class="inline h-40px w-40px"></IconVector1> Your
         </div>
-        <div class="mt-24px text-26px font-700 xl:mt-40px xl:text-40px">
-          探索新數位，重新定義未來
+        <div class="my-4 text-40px font-900 leading-46px tracking-[4%] font-raleway">
+          <IconVector2 class="mr-8px inline h-40px w-40px"></IconVector2>
+          <span class="text-primary-hover">Future</span>
+          <span class="ml-12px mr-8px border border-r-4px border-neutral-07 rounded-4px"></span>
+          <span class="px-10px">.</span>
         </div>
+        <div class="mt-24px heading-3">探索新數位，重新定義未來</div>
       </div>
-      <div class="mt-40px w-full xl:mt-0 xl:w-2/5">
-        <!-- 官方的還有bug -->
-        <!-- <dot-lottie :src="lottie" class="h-304px w-359px" autoplay loop></dot-lottie> -->
-        <Vue3Lottie
-          :animation-data="lottie"
-          class="max-h-304px max-w-359px"
-          auto-play
-          loop
-        ></Vue3Lottie>
-      </div>
-    </div>
-    <div>
-      <div className="mt-10 flex justify-center xl:justify-start items-center gap-x-6">
-        <a
-          href="#"
-          class="rounded-20px bg-primary px-24px py-16px text-17px text-black font-500 decoration-none shadow-sm hover:bg-primary-hover focus-visible:outline-2 focus-visible:outline-primary/70 focus-visible:outline-offset-2 focus-visible:outline"
+      <div class="w-full flex justify-center">
+        <button
+          type="submit"
+          class="rounded-full bg-primary p-14px text-neutral-07 hover:bg-primary-hover"
         >
-          瞭解更多 <IconArrowRight class="inline"></IconArrowRight>
-        </a>
+          <IconChavronUp class="h-32px w-32px rotate-180"></IconChavronUp>
+        </button>
       </div>
     </div>
-    <div class="mt-100px">
-      <Vue3Marquee :duration="15">
+
+    <div class="mt-124px">
+      <Vue3Marquee :duration="15" gradient gradientLength="15%">
         <img v-for="(item, index) in marqueeList" :key="index" :src="item" class="mx-40px" />
       </Vue3Marquee>
+    </div>
+
+    <div
+      class="absolute top-0 flex transform items-center justify-center -left-50% -translate-y-1/2"
+    >
+      <div class="placeholder" :style="{ width: eW + 'px', height: eW + 'px' }"></div>
+      <div
+        class="dashed-circle-bottom !border-primary"
+        :style="{ width: eW + 'px', height: eW + 'px' }"
+      ></div>
+      <div
+        class="dashed-circle-bottom !border-neutral-04"
+        :style="{
+          width: eW * 0.7 + 'px',
+          height: eW * 0.7 + 'px',
+        }"
+      ></div>
+    </div>
+    <div class="absolute flex transform items-center justify-center -right-50% -translate-y-1/2">
+      <div class="placeholder" :style="{ width: eW + 'px', height: eW + 'px' }"></div>
+      <div
+        class="dashed-circle-bottom !border-neutral-04"
+        :style="{ width: eW + 'px', height: eW + 'px' }"
+      ></div>
+      <div
+        class="dashed-circle-bottom !border-primary"
+        :style="{
+          width: eW * 0.8 + 'px',
+          height: eW * 0.8 + 'px',
+        }"
+      ></div>
+      <div
+        class="dashed-circle-bottom !border-neutral-04"
+        :style="{
+          width: eW * 0.6 + 'px',
+          height: eW * 0.6 + 'px',
+        }"
+      ></div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.title {
-  @apply text-28px font-400 font-rammetto-one lg:text-56px;
+@keyframes rotateAnimation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
-.fade-in-bottom {
-  -webkit-animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-  animation: fade-in-bottom 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+
+.dashed-circle-bottom {
+  @apply w-190px h-190px;
+  border: 2px dashed;
+  border-radius: 50%; /* 使邊框呈現圓形 */
+  position: absolute;
+  animation: rotateAnimation 150s linear infinite; /* 應用旋轉動畫，每次動畫持續 50 秒，無限次重復 */
+  margin: 20px; /* 為了更好的展示效果，添加一些外邊距 */
+  z-index: -1;
 }
 </style>
