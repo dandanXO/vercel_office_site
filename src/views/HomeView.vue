a9b85e969c46123fe24918fd0e5a5578ba629192
Frank <frank@ncfc.cc>
2024-02-19 23:49:25 +0800

    feat(section 2): rewrite

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,51 +2,23 @@
 import { imgUrl } from '@/assets'
 
 import HomeSection1 from './HomeView/components/HomeSection1.vue'
+import HomeSection2 from './HomeView/components/HomeSection2.vue'
 import HomeSection3 from './HomeView/components/HomeSection3.vue'
 import HomeSection4 from './HomeView/components/HomeSection4.vue'
 import HomeSection5 from './HomeView/components/HomeSection5.vue'
 import HomeSection6 from './HomeView/components/HomeSection6.vue'
 
 const windowsSize = useWindowSize()
-
 const section2Ref = ref(null)
-// const targetIsVisible = useElementVisibility(section2Ref)
 const section2Position = useElementBounding(section2Ref)
-const targetIsVisible = computed(() => section2Position.top.value < windowsSize.height.value)
+const section2Visible = computed(() => section2Position.top.value < windowsSize.height.value - 250)
 </script>
 
 <template>
   <div>
     <HomeSection1 />
     <!-- Section 2 -->
-    <div class="relative mt-87px bg-neutral-07 py-150px">
-      <img class="absolute bottom-0 left-0" :src="imgUrl('home/2-bg-1.png')" />
-      <img class="absolute right-0 top-0" :src="imgUrl('home/2-bg-2.png')" />
-      <div class="relative z-1 hidden text-center text-40px text-white font-700 xl:block">
-        定義全新的企業數字 DNA
-      </div>
-      <div
-        class="relative z-1 flex flex-wrap justify-between gap-60px xl:mt-80px xl:gap-0"
-        ref="section2Ref"
-      >
-        <div
-          class="w-full text-center opacity-0 xl:w-1/3"
-          :class="[
-            { [`fade-in-bottom-delay-${index}`]: targetIsVisible },
-            // `fade-in-bottom-delay-${index}`,
-          ]"
-          v-for="(item, index) in [
-            { num: '5', sub: '國家' },
-            { num: '100+', sub: '客戶' },
-            { num: '300+', sub: '員工' },
-          ]"
-          :key="index"
-        >
-          <div class="text-50px text-primary font-800 font-mulish">{{ item.num }}</div>
-          <div class="mt-16px text-20px text-neutral-04 font-400 font-mulish">{{ item.sub }}</div>
-        </div>
-      </div>
-    </div>
+    <HomeSection2 :section-visible="section2Visible" ref="section2Ref" class="my-40px xl:my-60px" />
     <!-- Section 3 -->
     <HomeSection3 />
     <!-- Section 4 -->
---
+++ src/views/HomeView/components/HomeSection2.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import IconVector from '@icons/nd/vector.svg'
+import { imgUrl } from '@/assets'
+const props = defineProps<{
+  sectionVisible: boolean
+}>()
+const img1 = imgUrl('home/2-1.png')
+
+const goals = ref([
+  {
+    num: 0,
+    numTarget: 5,
+    numStr: '',
+    sub: '國家',
+  },
+
+  { num: 0, numTarget: 100, numStr: '+', sub: '客戶' },
+  { num: 0, numTarget: 300, numStr: '+', sub: '員工' },
+])
+
+watch(
+  () => props.sectionVisible,
+  (val) => {
+    if (val) {
+      console.log('section2 is visible')
+      goals.value.forEach((goal) => {
+        // let c = goal.num
+        let updateCounter = () => {
+          let c = goal.num
+
+          let increment = goal.numTarget / 100
+
+          if (c < goal.numTarget) {
+            goal.num = Math.ceil(c + increment)
+            // goal.num = Math.round(Math.random() * 100)
+            // c = Math.ceil(c + increment)
+            setTimeout(updateCounter, 2) //單位:ms
+          } else {
+            goal.num = goal.numTarget
+          }
+        }
+        updateCounter()
+      })
+    }
+  }
+)
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center py-40px container xl-flex-row-reverse xl:gap-117.5px xl:py-60px"
+  >
+    <div class="flex flex-1 flex-col">
+      <div
+        class="flex flex-col items-center justify-center gap-40px py-76px xl:items-start xl:py-60px"
+      >
+        <div class="flex flex-col items-center justify-center xl:items-start">
+          <IconVector class="mb-16px h-43px w-43px" />
+          <div class="header-2 xl:header-1">我們的里程碑</div>
+        </div>
+        <div
+          class="flex items-center gap-10px xl:justify-start"
+          v-for="(item, index) in goals"
+          :key="index"
+          ref="sectionRef"
+        >
+          <span class="text-50px text-neutral-07 font-700 leading-55px font-raleway">{{
+            item.num + item.numStr
+          }}</span>
+          <span class="title-md">{{ item.sub }}</span>
+        </div>
+      </div>
+    </div>
+    <img :src="img1" class="h-408px rounded-20px object-cover" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
