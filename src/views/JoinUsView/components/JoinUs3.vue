fd20d7815a12ed971fb14b3130b977e3da7e2933
dan <dan@homway.tw>
2024-02-23 20:41:56 +0800

    feat: add join us

--- src/views/JoinUsView/components/JoinUs3.vue
+++ src/views/JoinUsView/components/JoinUs3.vue
@@ -93,7 +93,8 @@ const contentList = reactive([
 const Vector = imgUrl('joinUs/joinus3/Vector.svg')
 const arrowRight = imgUrl('joinUs/joinus3/arrow-right.svg')
 
-const handleExpand = (index) => {
+const handleExpand = (index = 0) => {
+  index ? index:0
   contentList[index].expand = !contentList[index].expand
 }
 </script>
@@ -101,42 +102,42 @@ const handleExpand = (index) => {
 <template>
   <div class="overflow-hidden bg-primary-bg text-gray-900">
     
-    <div class="relative mx-auto px-6 py-10 lg:px-20 justify-items-center">
+    <div class="relative mx-auto justify-items-center px-6 py-10 lg:px-20">
       <!-- Title and Description -->
       <div class="mb-5 flex flex-col items-center justify-center text-center">
         <img :src="Vector" class="mb-5 h-[42px] w-[42px]" />
         <h1 class="mb-5 text-4xl font-bold">加入我們</h1>
-        <p class="mb-3 text-neutral-06 text-xl">
+        <p class="mb-3 text-xl text-neutral-06">
           下方為我們目前正在招募的職缺，如果下方找不到你的職位，也歡迎來信自薦！
         </p>
       </div>
 
       <!-- Cards and Image Layout -->
-      <div class="flex justify-center items-center">
+      <div class="flex items-center justify-center">
         <table class="border-collapse" style="width: 1040px">
           <thead class="" style="background-color:#E4FF00;">
           <tr>
-            <th class="text-left py-2 px-2 text-base">職位名稱</th>
-            <th class="text-left py-2 px-2 text-base">類別</th>
-            <th class="text-left py-2 px-2 text-base">職位描述</th>
+            <th class="px-2 py-2 text-left text-base">職位名稱</th>
+            <th class="px-2 py-2 text-left text-base">類別</th>
+            <th class="px-2 py-2 text-left text-base">職位描述</th>
           </tr>
           </thead>
           <tbody>
-          <tr v-for="(item , index) in contentList" :v-key="index">
-            <td class="text-left py-2 px-2 w-2/12">
+          <tr v-for="(item , index) in contentList" :key="item.title">
+            <td class="w-2/12 px-2 py-2 text-left">
               <div class="text-base text-neutral-06 font-bold">{{item.jobName}}</div>
               <div class="font-bold">{{item.title}}</div>
 
             </td>
-            <td class="text-left py-2 px-2 w-1/12 text-sm font-normal">{{item.type}}</td>
-            <td class="text-left py-6 px-6 flex w-12/12 font-normal text-sm">
+            <td class="w-1/12 px-2 py-2 text-left text-sm font-normal">{{item.type}}</td>
+            <td class="w-12/12 flex px-6 py-6 text-left text-sm font-normal">
               <!--展开的 code start-->
-              <div v-if="item.expand" class="text-ellipsis  h-12" style="width:560px;height:auto;text-overflow:ellipsis;">
-                <div v-for="(mytext, mkey) in item.contentText" :v-key="mkey">
+              <div v-if="item.expand" class="h-12 text-ellipsis" style="width:560px;height:auto;text-overflow:ellipsis;">
+                <div v-for="(mytext, mkey) in item.contentText" :key="mkey">
                   {{mytext}}
                 </div>
-                <div class="mt-15 mb-5">
-                <button class="rounded-full py-4 px-4 m-5 ml-0 border flex text-base" style="background-color: transparent;border:1px solid #141414">
+                <div class="mb-5 mt-15">
+                <button class="m-5 ml-0 flex border rounded-full px-4 py-4 text-base" style="background-color: transparent;border:1px solid #141414">
                   投遞履歷 <img :src="arrowRight" class="h-6 w-6" />
                 </button>
                 </div>
@@ -146,13 +147,13 @@ const handleExpand = (index) => {
               <!--合起来的 code start-->
               <div v-if="!item.expand" class="h-20" style="width:560px;display: -webkit-box;overflow: hidden;-webkit-box-orient: vertical;-webkit-line-clamp: 4;">
 <!--                     {{item.contentText}}-->
-                <div v-for="(mytext, mkey) in item.contentText" :v-key="mkey">
+                <div v-for="(mytext, mkey) in item.contentText" :key="mkey">
                   {{mytext}}
                 </div>
               </div>
               <!--合起来的 code end-->
-              <div class="flex  justify-center" style="width: calc(100% - 560px);">
-                <div class="mb-2 flex items-center justify-center rounded-full p-6 text-white h-[60px] w-[60px] cursor-pointer" style="background-color: #141414" @click="handleExpand(index)">
+              <div class="flex justify-center" style="width: calc(100% - 560px);">
+                <div class="mb-2 h-[60px] w-[60px] flex cursor-pointer items-center justify-center rounded-full p-6 text-white" style="background-color: #141414" @click="handleExpand(index as number)">
                   <img :src="Vector" class="h-[96px] w-[96px]" :class="item.expand?'translationCircle':'translationCircleBack'"/>
                 </div>
               </div>
