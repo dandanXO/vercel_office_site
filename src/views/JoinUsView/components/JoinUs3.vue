4db2903e91b288086b79bad686628fb0dfec56dd
dan <dan@homway.tw>
2024-02-27 21:50:44 +0800

    feat: update css

--- src/views/JoinUsView/components/JoinUs3.vue
+++ src/views/JoinUsView/components/JoinUs3.vue
@@ -4,7 +4,7 @@ import { reactive } from 'vue'
 
 
 
-import { onMounted } from "vue";
+import { onMounted, nextTick } from "vue";
 import AOS from 'aos';
 
 onMounted(() => {
@@ -95,7 +95,12 @@ const arrowRight = imgUrl('joinUs/joinus3/arrow-right.svg')
 
 const handleExpand = (index = 0) => {
   index ? index:0
-  contentList[index].expand = !contentList[index].expand
+  nextTick().then(()=>{
+    
+  })
+  setTimeout(()=>{
+    contentList[index].expand = !contentList[index].expand
+  }, 200)
 }
 </script>
 
@@ -131,8 +136,9 @@ const handleExpand = (index = 0) => {
             </td>
             <td class="w-1/12 px-2 py-2 text-left text-sm font-normal">{{item.type}}</td>
             <td class="w-12/12 flex px-6 py-6 text-left text-sm font-normal">
+              
                 <!--展开的 code start-->
-                
+                <Transition name="drawer" key="1">
                   <div v-if="item.expand" class="h-12 text-ellipsis" style="width:560px;height:auto;text-overflow:ellipsis;">
                     <div v-for="(mytext, mkey) in item.contentText" :key="mkey">
                       {{mytext}}
@@ -143,17 +149,16 @@ const handleExpand = (index = 0) => {
                     </button>
                     </div>
                   </div>
-                
+                </Transition>
                 <!--展开的 code end-->
                 <!--合起来的 code start-->
-                
-                <div v-if="!item.expand" class="h-20" style="width:560px;display: -webkit-box;overflow: hidden;-webkit-box-orient: vertical;-webkit-line-clamp: 4;">
-  <!--                     {{item.contentText}}-->
-                  <div v-for="(mytext, mkey) in item.contentText" :key="mkey">
-                    {{mytext}}
+                <Transition name="drawer2" key="2">
+                  <div v-if="!item.expand" class="h-20" style="width:560px;display: -webkit-box;overflow: hidden;-webkit-box-orient: vertical;-webkit-line-clamp: 4;">
+                    <div v-for="(mytext, mkey) in item.contentText" :key="mkey">
+                      {{mytext}}
+                    </div>
                   </div>
-                </div>
-                
+                </Transition>
                 <!--合起来的 code end-->
               <div class="flex justify-center" style="width: calc(100% - 560px);">
                 <div class="mb-2 h-[60px] w-[60px] flex cursor-pointer items-center justify-center rounded-full p-6 text-white" style="background-color: #141414" @click="handleExpand(index as number)">
@@ -209,20 +214,59 @@ const handleExpand = (index = 0) => {
   transition: all 0.5s ease;
 }
 
-.drawer {
-  width: 0;
-  overflow: hidden;
-  transition: width 0.5s ease;
-  background-color: #f0f0f0;
-  /* 其他樣式 */
+.drawer-enter-active {
+  transition: all 2s ; 
+}
+// .drawer-leave-active {
+  
+// }
+.drawer-enter-to{
+  max-height: 100vh;
 }
 
-.drawer-enter-active, .drawer-leave-active {
-  width: 250px; /* 展開時的寬度 */
+.drawer-enter-from {
+  // transform: translateX(20px);
+  opacity: 0;
+  // transition: all 0.25s ease;
+  max-height: 0px;
+}
+.drawer-leave-to{
+  max-height: 100vh;
+  opacity: 0;
+  display: none;
+}
+.drawer-leave-from{
+  max-height: 0px;
+  opacity: 0;
+  display: none;
 }
 
-.drawer-enter, .drawer-leave-to /* Vue 2.1.8 版本及以上 */ {
-  width: 0;
+
+.drawer2-enter-active {
+  transition: all 2s ;
 }
+// .drawer2-leave-active {
+  
+// }
+.drawer2-enter-to{
+  max-height: 100px;
+}
+
+.drawer2-enter-from {
+  // transform: translateX(20px);
+  opacity: 0;
+  max-height: 0px;
+}
+.drawer2-leave-to{
+  max-height: 90vh;
+  opacity: 0;
+  display: none;
+}
+.drawer2-leave-from{
+  max-height: 0px;
+  opacity: 0;
+  display: none;
+}
+
 
 </style>
